eb30575d8a269eb51893c41cd96e1b2ace950057
cristina <cristinavelican@gmail.com>
2021-02-19 17:17:35 +0100

    added the search scenatio functionality

---
+++ features/search.feature
@@ -0,0 +1,9 @@
+Feature: Search
+
+This feature will deal with searching & validating the search of the products of this website
+
+@search
+Scenario: Validation of search functionality on home page
+Given We are on the home page of the automationpractice website
+When We search for "dress"
+Then The number of "dress" results should match the number of products displayed
---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,16 @@
+Given('We are on the home page of the automationpractice website') do
+    visit "http://automationpractice.com/index.php"
+end
+  
+When('We search for {string}') do |product_name|
+   fill_in 'search_query_top', with: product_name
+   find(:css,"button[name='submit_search']").click
+end
+  
+Then('The number of {string} results should match the number of products displayed') do |string|
+   number_of_results = find(:css,"span[class='heading-counter']").text.split()[0].to_i
+   p number_of_results
+   number_of_products_displayed = page.all(:css,".product_img_link").length
+   p number_of_products_displayed
+   expect(number_of_products_displayed).to eq(number_of_results)
+end
