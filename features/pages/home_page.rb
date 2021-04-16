306a2501c6b0dd387adff1f2f042f94ad33ed586
cristina <cristinavelican@gmail.com>
2021-04-16 13:38:48 +0200

    add code refactoring (POM)

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -1,6 +1,15 @@
 class HomePage < Screen
+  SEARCH_BAR_CSS  = 'search_query_top'
+  SEARCH_ICON_CSS =  'button[name="submit_search"]'
+
   include Helpers
 
- 
+ def search_product product_name
+  fill_in SEARCH_BAR_CSS, with: product_name
+ end
+
+ def click_search_icon
+  find(:css,SEARCH_ICON_CSS).click
+ end
 
 end
--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -3,11 +3,17 @@ Given('We are on the home page of the automationpractice website') do
 end
   
 When('We search for {string}') do |product_name|
+   #To get familiar with the framework run first using the non-refactor version
+   #When ready, uncomment the refactor lines (lines 11 and 12) that use the POM principles and run the scenario like that
    fill_in 'search_query_top', with: product_name
    find(:css,"button[name='submit_search']").click
+   
+   #@screen.homepage.search_product product_name
+   #@screen.homepage.click_search_icon
 end
   
 Then('The number of {string} results should match the number of products displayed') do |string|
+   #To get familiar with the framework run first using the non-refactor version
    number_of_results = find(:css,"span[class='heading-counter']").text.split()[0].to_i
    p number_of_results
    number_of_products_displayed = page.all(:css,".product_img_link").length
